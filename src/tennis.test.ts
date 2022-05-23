8c260b03bd378a48fe4ef38b4c6dfab81f9862fc
呂家綾 <slynnlin@lvjialingde-air.lan>
2022-05-23 08:33:48 +0800

    test(tennis): update testing

--- src/tennis.test.ts
+++ src/tennis.test.ts
@@ -20,6 +20,62 @@ describe("Tennis", () => {
       setPlayer1ScoreTimes(2);
       tobeScore("thirty love");
     });
+
+    it("should be forty love", () => {
+      setPlayer1ScoreTimes(3);
+      tobeScore("forty love");
+    });
+  });
+
+  describe("play2 score", () => {
+    it("should be love fifteen", () => {
+      setPlayer2ScoreTimes(1);
+      tobeScore("love fifteen");
+    });
+
+    it("should be love thirty", () => {
+      setPlayer2ScoreTimes(2);
+      tobeScore("love thirty");
+    });
+
+    it("should be love forty", () => {
+      setPlayer2ScoreTimes(3);
+      tobeScore("love forty");
+    });
+  });
+
+  describe("is same score", () => {
+    it("should be fifteen all", () => {
+      setPlayer1ScoreTimes(1);
+      setPlayer2ScoreTimes(1);
+      tobeScore("fifteen all");
+    });
+
+    it("should be thirty all", () => {
+      setPlayer1ScoreTimes(2);
+      setPlayer2ScoreTimes(2);
+      tobeScore("thirty all");
+    });
+
+    it("should be deuce (3:3)", () => {
+      setPlayer1ScoreTimes(3);
+      setPlayer2ScoreTimes(3);
+      tobeScore("deuce");
+    });
+
+    it("should be deuce (7:7)", () => {
+      setPlayer1ScoreTimes(7);
+      setPlayer2ScoreTimes(7);
+      tobeScore("deuce");
+    });
+  });
+
+  describe("is adv", () => {
+    it("should be Peter adv", () => {
+      setPlayer1ScoreTimes(4);
+      setPlayer2ScoreTimes(3);
+      tobeScore("Peter adv");
+    });
   });
 
   function tobeScore(expected) {
@@ -29,6 +85,12 @@ describe("Tennis", () => {
   function setPlayer1ScoreTimes(times: number) {
     for (let i = 1; i <= times; i++) {
       tennis.player1Score();
+    }
+  }
+
+  function setPlayer2ScoreTimes(times: number) {
+    for (let i = 1; i <= times; i++) {
+      tennis.player2Score();
     }
   }
 });
--- src/tennis.ts
+++ src/tennis.ts
@@ -1,16 +1,39 @@
 export class Tennis {
   play1Point = 0;
+  play2Point = 0;
   pointDictionary = {
+    0: "love",
     1: "fifteen",
     2: "thirty",
+    3: "forty",
   };
+
   constructor() {}
 
   score() {
-    if (this.play1Point === 1 || this.play1Point === 2) {
-      return `${this.pointDictionary[this.play1Point]} love`;
+    if (this.isSamePoint()) {
+      return this.play1Point < 3
+        ? `${this.pointDictionary[this.play1Point]} all`
+        : "deuce";
     }
+
+    const pointDifferent = Math.abs(this.play1Point - this.play2Point);
+    if (pointDifferent > 0 && pointDifferent <= 3) {
+      if (Math.abs(this.play1Point - this.play2Point) === 1) {
+        if (this.play1Point > this.play2Point) {
+          return `Peter adv`;
+        }
+      }
+
+      return `${this.pointDictionary[this.play1Point]} ${
+        this.pointDictionary[this.play2Point]
+      }`;
+    }
+
     return "love all";
   }
   player1Score = () => this.play1Point++;
+  player2Score = () => this.play2Point++;
+
+  private readonly isSamePoint = () => this.play1Point === this.play2Point;
 }
